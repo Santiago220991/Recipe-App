6073e7300c6a2b4dd4db9deeaf8233ad528b27e5
Santiago <alexander.cardenas10@outlook.com>
2022-08-29 19:54:40 -0500

    Create foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,30 @@
+class FoodsController < ApplicationController
+    def index
+        @foods=Food.where(user: current_user)
+    end
+
+    def new
+        @food=Food.new
+    end
+
+    def create
+        @food=Food.new(food_params)
+        @food.user_id=current_user.id
+        if @food.save
+            redirect_to foods_path,  notice: "Food added"
+        else
+           render :new
+        end
+    end
+
+    def destroy
+        @food=Food.find(params[:id])
+        if Food.destroy(@food.id)
+            redirect_to foods_path,  notice: "Food deleted"  
+        end
+    end
+
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+    end
+  end
