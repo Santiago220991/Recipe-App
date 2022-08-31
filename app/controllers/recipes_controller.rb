8b90c6473d66962edc9dc665671e4c34c8712395
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-30 20:17:08 -0500

    Add new and indexviews for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,34 @@
 class RecipesController < ApplicationController
-  def show; end
+  def index
+  @recipes = current_user.recipes.all
+end
+
+def create
+  @recipe = current_user.recipes.new(recipe_params)
+  if @recipe.save
+    flash[:success] = "Your recipe has been created!"
+    redirect_to recipes_path
+  else
+    flash[:error] = "Inventory not created!"
+    redirect_to new_recipe_path
+  end
+end
+
+def show
+  @recipe = current_user.recipes.find(params[:id])
+end
+
+def destroy
+  @recipe = current_user.recipes.find(params[:id])
+  @recipe.destroy
+  redirect_to recipes_path
+end
+
+private
+
+def recipe_params
+  params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+end
+
+
 end
