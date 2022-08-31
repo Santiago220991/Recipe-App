8aaec0b7aeb62f5fa00cd13a1809b93a857d3bb8
Santiago <alexander.cardenas10@outlook.com>
2022-08-31 11:44:11 -0500

    Fix shopping list to show missing food for the recipe

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,8 +1,16 @@
 class GeneralShoppingListController < ApplicationController
   def index
     @recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id])
+    @recipe_foods.each do |food|
+      if (Food.find(food.food_id).quantity-food.quantity < 1)
+        food.quantity=(Food.find(food.food_id).quantity-food.quantity)*(-1)
+      else
+        food.quantity=0
+      end
+    end
+    @recipe_foods=@recipe_foods.filter {|element| element.quantity!=0}
     @food_items = @recipe_foods.length
-    @total = 0
+    @total = 0   
     @recipe_foods.each do |food|
       @total += food.quantity * Food.find(food.food_id).price
     end
