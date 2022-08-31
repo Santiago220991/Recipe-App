d5ceb860e57a2a2b256d994557f0760c5dc53b7e
Santiago <alexander.cardenas10@outlook.com>
2022-08-31 15:15:17 -0500

    Check linters

--- spec/integration/food_index_spec.rb
+++ spec/integration/food_index_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Food', type: :feature do
-
-describe 'index page' do
-before(:each) do
-User.destroy_all
-Food.destroy_all
-Recipe.destroy_all
-RecipeFood.destroy_all
-user=User.create(name:"user", email:"user@gmail.com", password:123456)
-visit root_url
-fill_in "user_email", with: user.email
-fill_in "user_password", with: user.password
-click_button 'Log in'
-visit foods_path
-end
-it "should render List of Food title" do
-expect(page).to have_content("List of Food")
-end
-it "should render a Add Food link" do
-expect(page).to have_content("Add Food") 
-end
-it "should render a Log out button" do
-expect(page).to have_content("Log out")
-end
-it "should redirecto to new food page" do
-click_link 'Add Food'
-expect(page).to have_current_path(new_food_path)
-end
-end
+  describe 'index page' do
+    before(:each) do
+      User.destroy_all
+      Food.destroy_all
+      Recipe.destroy_all
+      RecipeFood.destroy_all
+      user = User.create(name: 'user', email: 'user@gmail.com', password: 123_456)
+      visit root_url
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit foods_path
+    end
+    it 'should render List of Food title' do
+      expect(page).to have_content('List of Food')
+    end
+    it 'should render a Add Food link' do
+      expect(page).to have_content('Add Food')
+    end
+    it 'should render a Log out button' do
+      expect(page).to have_content('Log out')
+    end
+    it 'should redirecto to new food page' do
+      click_link 'Add Food'
+      expect(page).to have_current_path(new_food_path)
+    end
+  end
 end
--- spec/integration/general_shopping_list_spec.rb
+++ spec/integration/general_shopping_list_spec.rb
@@ -1,40 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'General Shopping List', type: :feature do
+  describe 'index page' do
+    before(:each) do
+      User.destroy_all
+      Food.destroy_all
+      Recipe.destroy_all
+      RecipeFood.destroy_all
+      user = User.create(name: 'user', email: 'user@gmail.com', password: 123_456)
+      food = Food.create(name: 'meat', measurement_unit: 'lib', price: 1, quantity: 3, user:)
+      recipe = Recipe.create(name: 'bulgogi', preparation_time: 2, cooking_time: 1, description: 'good', public: true,
+                             user:)
+      RecipeFood.create(quantity: 5, recipe:, food:)
+      visit root_url
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit general_shopping_list_index_path(recipe_id: recipe.id)
+    end
+    it 'should render Shopping List title' do
+      expect(page).to have_content('Shopping List')
+    end
 
-describe 'index page' do
-before(:each) do
-User.destroy_all
-Food.destroy_all
-Recipe.destroy_all
-RecipeFood.destroy_all
-user=User.create(name:"user", email:"user@gmail.com", password:123456)
-food=Food.create(name:"meat", measurement_unit:"lib", price:1, quantity:3, user:user)
-recipe=Recipe.create(name:"bulgogi", preparation_time:2, cooking_time:1, description:"good", public:true, user:user)
-food_recipe=RecipeFood.create(quantity:5, recipe:recipe, food:food)
-visit root_url
-fill_in "user_email", with: user.email
-fill_in "user_password", with: user.password
-click_button 'Log in'
-visit general_shopping_list_index_path(recipe_id: recipe.id)
-end
-it "should render Shopping List title" do
-    
-expect(page).to have_content("Shopping List")
-end
+    it 'Ammount of food items to buy should be 1' do
+      expect(page).to have_content('Amount of food items to buy: 1')
+    end
 
-it "Ammount of food items to buy should be 1" do
+    it 'Total value of food needed: ' do
+      expect(page).to have_content('Total value of food needed: 2')
+    end
 
-    expect(page).to have_content("Amount of food items to buy: 1")
-end
-
-it "Total value of food needed: " do
-    expect(page).to have_content("Total value of food needed: 2")
-end
-
-it "Should rener a food called meat: " do
-    expect(page).to have_content("meat")
-end
-
-end
+    it 'Should rener a food called meat: ' do
+      expect(page).to have_content('meat')
+    end
+  end
 end
