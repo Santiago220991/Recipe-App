d8565ee77bd1e50cc41f8764f4a5f64ccda20c7a
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-30 21:27:38 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'public_recipes/index'
+  resources :public_recipes, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to:'foods#index'
   resources :foods, only:[:index, :new, :create, :destroy] do
