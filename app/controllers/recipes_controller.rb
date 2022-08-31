8bb218c6bf4ec079320c4327556b77b5555e8535
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-30 20:27:57 -0500

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,34 +1,32 @@
 class RecipesController < ApplicationController
   def index
-  @recipes = current_user.recipes.all
-end
-
-def create
-  @recipe = current_user.recipes.new(recipe_params)
-  if @recipe.save
-    flash[:success] = "Your recipe has been created!"
-    redirect_to recipes_path
-  else
-    flash[:error] = "Inventory not created!"
-    redirect_to new_recipe_path
+    @recipes = current_user.recipes.all
   end
-end
 
-def show
-  @recipe = current_user.recipes.find(params[:id])
-end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:success] = 'Your recipe has been created!'
+      redirect_to recipes_path
+    else
+      flash[:error] = 'Inventory not created!'
+      redirect_to new_recipe_path
+    end
+  end
 
-def destroy
-  @recipe = current_user.recipes.find(params[:id])
-  @recipe.destroy
-  redirect_to recipes_path
-end
+  def show
+    @recipe = current_user.recipes.find(params[:id])
+  end
 
-private
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
 
-def recipe_params
-  params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-end
+  private
 
-
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
